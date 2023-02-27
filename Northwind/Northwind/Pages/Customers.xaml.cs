0639e81f4a0efc099c5e3ef8c083a0133c80b6b4
Matthew Covey <w0246645@campus.nscc.ca>
2023-02-27 16:25:28 -0400

    New WPF Demo with Northwind DB using pages and datagrids.

---
+++ Northwind/Northwind/Pages/Customers.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Northwind.Pages
+{
+    /// <summary>
+    /// Interaction logic for Customers.xaml
+    /// </summary>
+    public partial class Customers : Page
+    {
+
+        private readonly NorthwindContext _context = new NorthwindContext();
+        private CollectionViewSource customerViewSource;
+        public Customers()
+        {
+            InitializeComponent();
+            customerViewSource = (CollectionViewSource)FindResource(nameof(customerViewSource));
+            
+        }
+
+        private void Page_Loaded(Object sender, RoutedEventArgs e)
+        {
+            _context.Customers.Load();
+
+            customerViewSource.Source = _context.Customers.Local.ToObservableCollection();
+        }
+    }
+}
